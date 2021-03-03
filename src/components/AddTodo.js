30ec6477c757ded70c496c76201167a7d49d3e74
Bishwajit <bishwa420@gmail.com>
2021-03-03 07:32:04 +0600

    - Few basic components added - Redux skeleton added

---
+++ src/components/AddTodo.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+class AddTodo extends Component{
+
+    render() {
+        return <div>AddTodo</div>;
+    }
+}
+
+export default AddTodo;
---
+++ src/components/VisibilityFilters.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+class VisibilityFilters extends Component{
+
+    render() {
+        return <div>VisibilityFilters</div>;
+    }
+}
+
+export default VisibilityFilters;
